b0eaf400696384fadc4e9502102b1fb43713f8ed
Sak-Ar10 <thakur.saksham707@gmail.com>
2026-09-12 09:41:38 +0530

    Add Week 6 practice and assignment problems

---
+++ week6/Assignment/BookInventory.java
@@ -0,0 +1,45 @@
+public class BookInventory {
+
+    private int copiesTotal;
+    private int copiesAvailable;
+
+    public BookInventory(int copiesTotal) {
+        if (copiesTotal <= 0) {
+            throw new IllegalArgumentException("construction rejected");
+        }
+        this.copiesTotal = copiesTotal;
+        this.copiesAvailable = copiesTotal;
+    }
+
+    public void checkOut() {
+        if (this.copiesAvailable > 0) {
+            this.copiesAvailable--;
+        }
+    }
+
+    public void checkIn() {
+        if (this.copiesAvailable < this.copiesTotal) {
+            this.copiesAvailable++;
+        }
+    }
+
+    public int getCopiesAvailable() {
+        return this.copiesAvailable;
+    }
+
+    public static void main(String[] args) {
+        BookInventory b = new BookInventory(3);
+        
+        b.checkOut(); 
+        b.checkOut(); 
+        b.checkOut();
+        b.checkOut(); 
+        System.out.println(b.getCopiesAvailable());
+
+        b.checkIn(); 
+        b.checkIn(); 
+        b.checkIn();
+        b.checkIn(); 
+        System.out.println(b.getCopiesAvailable());
+    }
+}
